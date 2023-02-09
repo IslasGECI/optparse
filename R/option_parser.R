09233c0871434842102dc0a1f4e9bf6a7d26d98a
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2023-02-09 12:38:13 -0800

    ✨ Add 'predictions_path' option

--- R/option_parser.R
+++ R/option_parser.R
@@ -9,6 +9,13 @@ get_options <- function() {
       help = "",
       metavar = "character",
       type = "character"
+    ),
+    optparse::make_option(
+      c("-p", "--predictions_path"),
+      default = "reports/non-tabular/summary_predictions.json",
+      help = "",
+      metavar = "character",
+      type = "character"
     )
   )
   opt_parser <- optparse::OptionParser(option_list = listaOpciones)
