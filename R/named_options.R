94b3434f9e9305ca41dba7c0bec6670c40fa156a
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2024-03-06 12:48:11 -0800

    🚧 First try

---
+++ R/named_options.R
@@ -0,0 +1,32 @@
+get_options_from_names <- function(names) {
+  listaOpciones <- make_lista_opciones(names)
+  opt_parser <- optparse::OptionParser(option_list = listaOpciones)
+  opciones <- optparse::parse_args(opt_parser)
+  return(opciones)
+}
+
+opcion_b_number <- optparse::make_option(
+  c("-b", "--b-number"),
+  default = "2000",
+  help = "Number of resample in bootstrapping method",
+  type = "integer"
+)
+
+opcion_output <- optparse::make_option(
+  c("-o", "--output_path"),
+  default = "results/output.csv",
+  help = "Output path",
+  metavar = "character",
+  type = "character"
+)
+
+OPTIONS <- list(
+  "b-number" = opcion_b_number,
+  "output" = opcion_output
+)
+
+make_lista_opciones <- function(names) {
+  n_names <- length(names_options_cli)
+  lista_opciones <- comprehenr::to_list(for (i in 1:n_names) OPTIONS[[names[i]]])
+  return(lista_opciones)
+}
